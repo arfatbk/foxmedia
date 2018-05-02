f199eb4bc7d50ce9860d5552c5f7c2c6f5f6172b
Arfat Bin Kileb <arfatbk@gmail.com>
2018-05-02 10:35:00 -0700

    Updated technologies and animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,14 @@ $(document).ready(function(){
     $('#contact').on('click', function() {
         $('.cover').css('visibility','visible');
         $('.cover').css('opacity',1);
+        $('.popover-send').css('top','50%');
     });
 
     $('#close-popover').on('click', function() {
         $('.cover').css('visibility','hidden');
         $('.cover').css('opacity',0);
+        
+        $('.popover-send').css('top','0');
     });
     
     $('#close-success-popover').on('click', function() {
