512b4800f32f7fed606659f7687920ff88a18df4
Arfat Bin Kileb <arfatbk@gmail.com>
2018-05-03 11:10:38 -0700

    Hero Contact function working

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,8 +12,55 @@ $(document).ready(function(){
         $('.popover-send').css('top','0');
     });
     
-    $('#close-success-popover').on('click', function() {
+    $('.close-success-popover').on('click', function() {
         $('.cover').css('visibility','hidden');
         $('.cover').css('opacity',0);
+        $('.popover-send').removeClass('remove');
+        $('.success-popover').addClass('remove');
+    });
+
+
+
+
+    // Call to Django mail
+
+    $(document).on('submit', '#sendMsgForm', function(e) {
+        e.preventDefault();
+        // if($('#name').val() =='' && $('#email').val()=='' && $('#message').val()==''){
+        //      ;
+        // }
+        $("#formID").validate();
+        //Disable send button
+        $('.submitBtn').prop('disabled', true);
+        $('.submitBtn').html(' Please wait..Sending message');
+
+        
+            $.ajax({
+                type:'POST',
+                url:'http://localhost:8000/send/message/',
+                data:{
+                    name:$('#name').val(),
+                    email:$('#email').val(),
+                    message:$('#message').val(),
+                },
+            }).done(function(){
+                //display success card
+                    $('.popover-send').addClass('remove');
+                    $('.success-popover').removeClass('remove');
+                    
+                    $('.submitBtn').prop('disabled', false);
+                    
+                    $('.submitBtn').html('Send message');
+
+            }).fail(function(){
+                    $('.submitBtn').prop('disabled', false);
+                    $('.popover-send').removeClass('remove');
+                    $('.success-popover').addClass('remove');
+                    
+                    $('.submitBtn').html('Send message');
+            });
     });
 });
+
+
+
